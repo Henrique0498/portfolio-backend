f1dbcd6371ff53378bb2ebf64c75a5fc4763f13c
Henrique Lopes <h.lopes.silva2015@gmail.com>
2024-07-20 00:19:42 -0300

    Feat: getToken

---
+++ src/helpers/get-token.ts
@@ -0,0 +1,35 @@
+import { ExecutionContext } from '@nestjs/common'
+import { GqlExecutionContext } from '@nestjs/graphql'
+import { Request } from 'express'
+
+export class GetToken {
+  toContext(context: ExecutionContext) {
+    const typeRequest = context.getType()
+    let token = context.switchToHttp().getRequest().cookies.token as string
+
+    if (!token && typeRequest === 'http') {
+      const request = context.switchToHttp().getRequest()
+      token = this.toRequest(request)
+    }
+    if (!token) {
+      const gqlContext = GqlExecutionContext.create(context).getContext()
+
+      token = this.toArray(gqlContext.req.rawHeaders)
+    }
+
+    return token
+  }
+
+  toRequest(request: Request) {
+    const [type, token] = request?.headers?.authorization?.split(' ') ?? []
+
+    return type === 'Bearer' ? token : null
+  }
+
+  private toArray(value: string[]) {
+    const findToken = value.find((item) => item.includes('Bearer'))
+    const [type, token] = findToken.split(' ') ?? []
+
+    return type === 'Bearer' ? token : null
+  }
+}

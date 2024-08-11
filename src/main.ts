ff51ae38e95b31d038d7c82e7839879badba3dd2
Henrique Lopes <h.lopes.silva2015@gmail.com>
2024-08-11 20:38:10 -0300

    Change: Cors

--- src/main.ts
+++ src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
   app.enableCors({
-    origin: process.env.FRONTEND_URL,
+    origin: [process.env.FRONTEND_URL_REACT, process.env.FRONTEND_URL_VUE],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true
   })

70e5d2dc90d8956d0d24385321d2b23dbc6b1d5f
Henrique0498 <h.lopes.silva2015@gmail.com>
2025-11-10 11:24:43 -0300

    change(update): atualizando packages

--- src/auth/pipes/auth-post-public.pipe.ts
+++ src/auth/pipes/auth-post-public.pipe.ts
@@ -9,11 +9,7 @@ import { IS_PUBLIC_KEY } from 'src/services/is-public'
 @Injectable()
 export class AuthValidatePublicPipe implements PipeTransform {
   transform(value: InAuthPostPublic) {
-    if (
-      !value.publicKey ||
-      !value.ip ||
-      value.publicKey.replaceAll('\\$', '$') !== IS_PUBLIC_KEY
-    ) {
+    if (!value?.publicKey || !value?.ip || value?.publicKey !== IS_PUBLIC_KEY) {
       throw new UnauthorizedException('You are not authorized to do this')
     }
 
